2d5b430530ca56ecb3e1c86a19374c7baf0c86a6
Thibaut <laro>
2020-12-12 17:05:39 +0100

    ouvertue du fichier ECAM4E dans assets le spinner des groupes fonctionne mais présence de doublons

--- app/src/main/java/com/example/qrcodeapp/ChoisirGroupe.java
+++ app/src/main/java/com/example/qrcodeapp/ChoisirGroupe.java
@@ -15,34 +15,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
-    //attributs
-    List<String> mTD = new ArrayList<String>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choisir_groupe);
+
         //attibuts affichage
         Button mButtonp12 = (Button) findViewById(R.id.groupe_choisi);
         Spinner mSpinnerG = (Spinner) findViewById(R.id.spinnergroupe);
+        Spinner mSpinnerP = (Spinner) findViewById(R.id.spinnerpromo);
 
+        //creation de la liste des Promotions
+        String [] listePromo;
+        try {
+            listePromo = choixPromo();
+        } catch (IOException e) {
+            e.printStackTrace();
+            listePromo= new String[]{"erreur"};
+        }
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        mSpinnerP.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the TD list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,listePromo);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        mSpinnerP.setAdapter(aa);
 
         //creation de la liste TD
-        mTD=choixGroupe("promotion/ECAM4E.txt");
-        String[] listeTD = mTD.toArray(new String[0]);
+        String[] listeTD = choixGroupe("promotion/ECAM4E.txt").toArray(new String[0]);
         //String[] listeTD ={"A","B","C"};
         //Getting the instance of Spinner and applying OnItemSelectedListener on it
         mSpinnerG.setOnItemSelectedListener(this);
         //Creating the ArrayAdapter instance having the TD list
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,listeTD);
+        ArrayAdapter aaa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,listeTD);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
-        mSpinnerG.setAdapter(aa);
+        mSpinnerG.setAdapter(aaa);
+
 
 
 
@@ -58,8 +74,10 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
 
     }
 
+
     private List<String> choixGroupe(String nomFichier)
     {
+        //prend en entré le nom du fichier de la promotion dans assets, et retourne une ArrayList contenant les groupes de TD possibles
         List<String> liste= new ArrayList<String>();
         liste.add("CM");
         BufferedReader reader = null;
@@ -68,34 +86,47 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
                     new InputStreamReader(getAssets().open(nomFichier), "UTF-8"));
             // do reading, usually loop until end of file reading
             String mLine;
+            int cpt=0;
             while ((mLine = reader.readLine()) != null) {
                 //process line
                 //récupération des groupes TD
                 String[] donnees = mLine.split(";");
                 String TD = donnees[3];
-                boolean ajouter;
-                ajouter = true;
-                for (int i = 0; i <liste.size(); i++)
-                {
-                    String elem= liste.get(i);
-                    if (TD == elem || TD == "TD") {ajouter = false;}
-                }
-                if (ajouter) {liste.add(TD);}
+                if(cpt>0){liste.add("TD"+TD);}
+                cpt++;
             }
         } catch (IOException e) {
             //log the exception
-            liste.add("bug");
         } finally {
             if (reader != null) {
                 try {
                     reader.close();
                 } catch (IOException e) {
                     //log the exception
+                    liste.add("erreur");
                 }
             }
         }
-        return liste;
+        return supDoublons(liste);
     }
+
+
+
+    private String [] choixPromo() throws IOException
+    {
+        String[] nomFichier = this.getAssets().list("promotion");
+        return nomFichier;
+    }
+
+
+    private List<String> supDoublons(List<String> L)
+    {
+        Set set = new HashSet() ;
+        set.addAll(L) ;
+        ArrayList Lbis = new ArrayList(set) ;
+        return Lbis;
+    }
+
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
