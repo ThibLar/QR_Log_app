8a8d8c28a13e652ad04457a2ac22a4672da1c04f
Maelle <maelle@maison>
2020-12-12 18:43:47 +0100

    Pour remettre le fichier csv à zéro au démarrage de l'application

--- app/src/main/java/com/example/qrcodeapp/Accueil.java
+++ app/src/main/java/com/example/qrcodeapp/Accueil.java
@@ -1,11 +1,14 @@
 package com.example.qrcodeapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileOutputStream;
 
 public class Accueil extends AppCompatActivity {
 
@@ -21,6 +24,16 @@ public class Accueil extends AppCompatActivity {
             public void onClick(View v){
                 Intent lanceActivityIntent = new Intent(Accueil.this, ChoisirGroupe.class);
                 startActivity(lanceActivityIntent);
+                try { ///data/data/com.example.qrcodeapp/files/listelecture.csv
+                    String file_out = "listelecture.csv";
+                    FileOutputStream fOut = openFileOutput(file_out, Context.MODE_PRIVATE);
+                    /* fini : on ferme le fichier */
+                    fOut.close();
+
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
         });
     }
