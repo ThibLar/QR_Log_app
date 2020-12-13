a9f29f8c7334f3f6ae943f6fde63fad63c3784d9
Thibaut <laro>
2020-12-13 16:54:35 +0100

    enregistrement des personnes devant etre presentes problemes de recuperation des valeurs des spinners

--- app/src/main/java/com/example/qrcodeapp/ChoisirGroupe.java
+++ app/src/main/java/com/example/qrcodeapp/ChoisirGroupe.java
@@ -1,5 +1,6 @@
 package com.example.qrcodeapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,11 +8,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -21,6 +24,8 @@ import java.util.Set;
 
 public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    String promoChoisie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,6 +36,8 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
         Button mButtonp12 = (Button) findViewById(R.id.groupe_choisi);
         Spinner mSpinnerG = (Spinner) findViewById(R.id.spinnergroupe);
         Spinner mSpinnerP = (Spinner) findViewById(R.id.spinnerpromo);
+        TextView mtextView = (TextView)findViewById(R.id.Autres);
+
 
         //creation de la liste des Promotions
         String [] listePromo;
@@ -48,8 +55,10 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
         //Setting the ArrayAdapter data on the Spinner
         mSpinnerP.setAdapter(aa);
 
+        String promoChoisie= mSpinnerP.getSelectedItem().toString();
+
         //creation de la liste TD
-        String[] listeTD = choixGroupe("promotion/ECAM4E.txt").toArray(new String[0]);
+        String[] listeTD = choixGroupe("promotion/"+promoChoisie).toArray(new String[0]);
         //String[] listeTD ={"A","B","C"};
         //Getting the instance of Spinner and applying OnItemSelectedListener on it
         mSpinnerG.setOnItemSelectedListener(this);
@@ -58,9 +67,9 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         mSpinnerG.setAdapter(aaa);
+        String groupeChoisi= mSpinnerP.getSelectedItem().toString();
 
-
-
+        creationGroupe(promoChoisie, "TD B", mtextView);
 
         mButtonp12.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -92,7 +101,7 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
                 //récupération des groupes TD
                 String[] donnees = mLine.split(";");
                 String TD = donnees[3];
-                if(cpt>0){liste.add("TD"+TD);}
+                if(cpt>0){liste.add("TD "+TD);}
                 cpt++;
             }
         } catch (IOException e) {
@@ -108,6 +117,74 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
             }
         }
         return supDoublons(liste);
+    }
+
+    private void creationGroupe(String nomFichier, String nomGroupe, TextView txt)
+    {
+        //prend en entré le nom du fichier de la promotion dans assets, et le nom du groupe (Cm, TD A, TD B,...)
+        // et enregistre un fichier contenant les personnes devant assister au cours
+        String listeEmail;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("promotion/"+ nomFichier), "UTF-8"));
+            // do reading, usually loop until end of file reading
+            String mLine;
+            int cpt=0;
+            listeEmail="";
+            while ((mLine = reader.readLine()) != null) {
+                //process line
+                //récupération des groupes TD
+                String[] donnees = mLine.split(";");
+                String TD = donnees[3];
+                String email = donnees[5];
+
+                if(cpt>0)
+                {
+                    if(nomGroupe.equals("TD "+TD))
+                    {
+                        listeEmail=listeEmail.concat(email+"\n");
+                    }
+
+                    else
+                    {
+                        if(nomGroupe=="CM")
+                        {
+                            listeEmail=listeEmail.concat(email+"\n");
+                        }
+                    }
+                }
+                cpt++;
+            }
+        } catch (IOException e) {
+            //log the exception
+            listeEmail="bug dans lecture fichier";
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+
+                }
+            }
+        }
+        //on souhaite enregistrer le fichier contenant les email des personnes attendues au cours
+        try {
+            txt.setText(listeEmail);
+            String file_out = "personnesAttendues.csv";
+            FileOutputStream fOut = openFileOutput(file_out, Context.MODE_PRIVATE);
+            fOut.write(listeEmail.getBytes());
+            /* fini : on ferme le fichier */
+            fOut.close();
+            //finish();
+
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            txt.setText("bug enrigestrement fichier");
+        }
     }
 
 
@@ -143,3 +220,4 @@ public class ChoisirGroupe extends AppCompatActivity implements AdapterView.OnIt
         // TODO Auto-generated method stub
     }
 }
+//spinner.setOnItemSelectedListener(new OnItemSelectedListener() {​​​​
