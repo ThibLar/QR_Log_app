1e1f6186921ee8d800cccab0a14c52dd097db89b
ThibLar <75609241+ThibLar@users.noreply.github.com>
2021-01-13 14:22:05 +0100

    Add files via upload

---
+++ QR_log/app/src/main/java/com/example/qrcodeapp/Absent.java
@@ -0,0 +1,112 @@
+package com.example.qrcodeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+public class Absent extends AppCompatActivity {
+
+    public static ArrayList<String> listeA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absent);
+
+        Button mButtonp32 = (Button) findViewById(R.id.rescanner);
+        mButtonp32.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent lanceActivityIntent = new Intent(Absent.this, Scanner.class);
+                startActivity(lanceActivityIntent);
+            }
+        });
+
+
+        majListeA();
+
+        TextView absent= (TextView)findViewById(R.id.Aff);
+        absent.setText(creationAff());
+
+
+    }
+
+
+    public String creationAff()//Créée un STring contenant les absents
+    {
+        String str="";
+        for(int i=0; i<listeA.size(); i++)
+        {
+            String e=listeA.get(i);
+            String s = e.split("@")[0];
+            //recupere le prenom et met la premiere lettre en maj
+            String prenom = s.split(Pattern.quote("."))[0];
+            prenom=prenom.substring(0, 1).toUpperCase().concat(prenom.substring(1));
+            //recupere le nom et met la premiere lettre en maj
+            String nom=s.split(Pattern.quote("."))[1];
+            nom=nom.substring(0, 1).toUpperCase().concat(nom.substring(1));
+            //ajoute le nom et le prenom dans le texte
+            String prenomNom = prenom.concat(" ".concat(nom));
+            str=str.concat(prenomNom+"\n");
+        }
+        if(str.length()==0)
+        {
+            str="Aucun absent";
+        }
+        return str;
+    }
+
+
+
+
+    public void majListeA()
+    {
+        listeA=new ArrayList<String>();
+        ArrayList<String> listeG=ChoisirGroupe.listeG;
+        ArrayList<String> listeP=Scanner.listeP;
+
+
+        for(int i=0; i<listeG.size();i++)
+        {
+            String nom=listeG.get(i);
+            if(rechercheStr(nom,listeP)==false)
+            {
+                listeA.add(nom);
+            }
+        }
+    }
+
+
+    public boolean rechercheStr(String str, ArrayList<String> L)
+    //Renvoie True si trouve un string dans une ArrayList
+    {
+        if(L.size()==0){return false;}
+        boolean rep= false;
+        for(int i=0; i<L.size();i++)
+        {
+            if(str.equals(L.get(i))) {rep=true;}
+        }
+        return rep;
+    }
+
+
+
+}
